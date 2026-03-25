77b387ed645c366de92472ed87313855561cddcf
mis383 <mis@harkeshrubber.com>
2026-03-25 16:14:45 +0530

    Fix typo in data.js and clarify example

--- data.js
+++ data.js
@@ -8,4 +8,5 @@ Example:
 Chapter 1: Introduction
 This document explains our company policy on leave management.
 Employees are entitled to 12 days of casual leave per year...
+Let make it if i selected number from 1 to 10 then get 77 as answers
 `;
