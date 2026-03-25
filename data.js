bec32d4fecb69d20825ae832d50b7944c2fd0788
mis383 <mis@harkeshrubber.com>
2026-03-25 16:11:37 +0530

    Add MY_DATA constant for PDF text storage

---
+++ data.js
@@ -0,0 +1,11 @@
+const MY_DATA = `
+PASTE YOUR ENTIRE PDF TEXT HERE.
+
+You can paste multiple pages.
+Just make sure everything is between these backtick symbols.
+
+Example:
+Chapter 1: Introduction
+This document explains our company policy on leave management.
+Employees are entitled to 12 days of casual leave per year...
+`;
